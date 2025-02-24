19fc13a4aecad858f6ca9fd57250edcc725afb7a
Pedro Filipe <diczs657@dentsplysirona.com>
2025-02-24 09:57:02 +0000

    fix mappings in nvim-tree

--- lua/user/nvim_tree.lua
+++ lua/user/nvim_tree.lua
@@ -7,15 +7,25 @@ if not ok then
 	return
 end
 
+local function my_on_attach(bufnr)
+	local api = require "nvim-tree.api"
+
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
+
+	-- default mappings
+	api.config.mappings.default_on_attach(bufnr)
+
+	-- custom mappings
+	vim.keymap.set('n', 'u', api.tree.change_root_to_parent, opts('Up'))
+end
+
 config.setup({
+	on_attach = my_on_attach,
 	sort_by = "case_sensitive",
 	view = {
 		adaptive_size = true,
-		mappings = {
-			list = {
-				{ key = "u", action = "dir_up" },
-			},
-		},
 	},
 	renderer = {
 		group_empty = true,
